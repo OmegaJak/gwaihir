0a2852f24c3964cd5a19a713d7421db3759c6927
OmegaJak <jak.kruger@gmail.com>
2023-08-10 17:09:58 -0500

    Remove/rename wasm and eframe template stuff

--- src/main.rs
+++ src/main.rs
@@ -23,7 +23,7 @@ fn main() -> eframe::Result<()> {
         LockStatusSensorBuilder::new().set_event_loop_builder(&mut native_options);
     let (monitor_handle, tx_to_monitor, rx_from_monitor) = create_sensor_monitor_thread();
     eframe::run_native(
-        "eframe template",
+        "Gwaihir",
         native_options,
         Box::new(move |cc| {
             let ctx_clone = cc.egui_ctx.clone();
@@ -40,24 +40,4 @@ fn main() -> eframe::Result<()> {
     )?;
     monitor_handle.join().unwrap();
     Ok(())
-}
-
-// When compiling to web using trunk:
-#[cfg(target_arch = "wasm32")]
-fn main() {
-    // Redirect `log` message to `console.log` and friends:
-    eframe::WebLogger::init(log::LevelFilter::Debug).ok();
-
-    let web_options = eframe::WebOptions::default();
-
-    wasm_bindgen_futures::spawn_local(async {
-        eframe::WebRunner::new()
-            .start(
-                "the_canvas_id", // hardcode it
-                web_options,
-                Box::new(|cc| Box::new(eframe_template::TemplateApp::new(cc))),
-            )
-            .await
-            .expect("failed to start eframe");
-    });
 }
