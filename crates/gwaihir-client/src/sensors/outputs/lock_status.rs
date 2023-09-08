2d271f748db7144f7074b0e9fd67a5f61c5de17a
serious573 <evens208@umn.edu>
2023-09-08 14:02:12 -0500

    Simplifying lock status UI

--- crates/gwaihir-client/src/sensors/outputs/lock_status.rs
+++ crates/gwaihir-client/src/sensors/outputs/lock_status.rs
@@ -2,7 +2,9 @@ use egui::{CollapsingHeader, Color32, RichText};
 use gwaihir_client_lib::UniqueUserId;
 use serde::{Deserialize, Serialize};
 
-use crate::sensors::outputs::sensor_output::SensorWidget;
+use crate::{
+    sensors::outputs::sensor_output::SensorWidget, ui_extension_methods::UIExtensionMethods,
+};
 
 #[derive(Serialize, Deserialize, Clone, PartialEq, Debug)]
 pub struct LockStatus {
@@ -21,22 +23,20 @@ impl Default for LockStatus {
 
 impl SensorWidget for LockStatus {
     fn show(&self, ui: &mut egui::Ui, id: &UniqueUserId) {
-        self.show_overall(ui);
         self.show_details(id, ui);
     }
 }
 
 impl LockStatus {
-    fn show_overall(&self, ui: &mut egui::Ui) {
-        if self.num_locks > self.num_unlocks {
-            ui.label(RichText::new("Currently Locked").color(Color32::RED));
-        } else {
-            ui.label(RichText::new("Currently Unlocked").color(Color32::DARK_GREEN));
-        }
-    }
-
     fn show_details(&self, id: &UniqueUserId, ui: &mut egui::Ui) {
-        CollapsingHeader::new("Locks/Unlocks")
+        let mut headerText = RichText::new("Currently Unlocked").color(Color32::DARK_GREEN);
+        if self.num_locks > self.num_unlocks {
+            headerText = RichText::new("Currently Locked").color(Color32::RED);
+        }
+
+        let layout_job = ui.create_default_layout_job(vec![headerText]);
+
+        CollapsingHeader::new(layout_job)
             .id_source(format!("{}_locks", id.as_ref()))
             .show(ui, |ui| {
                 ui.label(format!("Times Locked: {}", self.num_locks));
