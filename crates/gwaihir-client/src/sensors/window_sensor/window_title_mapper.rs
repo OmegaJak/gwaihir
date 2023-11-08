70e0a7c292d8efb0267a95d07f8f5c5009306621
OmegaJak <jak.kruger@gmail.com>
2023-11-08 16:45:08 -0600

    WIP mapping stuff

--- crates/gwaihir-client/src/sensors/window_sensor/window_title_mapper.rs
+++ crates/gwaihir-client/src/sensors/window_sensor/window_title_mapper.rs
@@ -1,12 +1,34 @@
 use linked_hash_set::LinkedHashSet;
+use nutype::nutype;
+use regex::Regex;
 use serde::{Deserialize, Serialize};
-use std::sync::{Arc, RwLock};
+use std::{
+    collections::HashMap,
+    sync::{Arc, RwLock},
+};
 
 use super::active_window_provider::WindowIdentifiers;
 
+#[nutype(derive(Serialize, Deserialize, AsRef, Clone, Into, Debug, Hash, PartialEq, Eq))]
+pub struct AppName(String);
+
+#[nutype(derive(Serialize, Deserialize, AsRef, Clone, Into, Debug, Hash, PartialEq, Eq))]
+pub struct OriginalTitle(String);
+
+#[nutype(derive(Serialize, Deserialize, AsRef, Clone, Into, Debug))]
+pub struct MappedTitle(String);
+
+#[derive(Serialize, Deserialize)]
+pub struct AcceptedMapping {
+    accepted_examples: HashMap<OriginalTitle, MappedTitle>,
+
+    #[serde(with = "serde_regex_as_str")]
+    regex: Regex,
+}
+
 #[derive(Serialize, Deserialize, Default)]
 pub struct WindowTitleMappings {
-    // TODO
+    mappings: HashMap<AppName, AcceptedMapping>,
 }
 
 impl WindowTitleMappings {
@@ -34,5 +56,24 @@ impl WindowTitleMapper {
 
     pub fn iter_pending(&self) -> linked_hash_set::Iter<'_, WindowIdentifiers> {
         self.pending_mappings.iter()
+    }
+}
+
+mod serde_regex_as_str {
+    use regex::Regex;
+
+    pub fn serialize<S>(regex: &Regex, ser: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        ser.serialize_str(regex.as_str())
+    }
+
+    pub fn deserialize<'de, D>(d: D) -> Result<Regex, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        let s: &str = serde::de::Deserialize::deserialize(d)?;
+        Regex::new(s).map_err(|e| serde::de::Error::custom(e.to_string()))
     }
 }
